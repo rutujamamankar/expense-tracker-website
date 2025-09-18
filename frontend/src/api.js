c78efd0938fca7296ce095a57b4b7040921a50c5
Rutuja Mamankar <rutujamamankar407@gmail.com>
2025-09-18 20:25:59 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const API = axios.create({
-  baseURL: `http://localhost:5000/api`,
+  baseURL:"https://expense-tracker-website-1-z9y1.onrender.com",
 });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
