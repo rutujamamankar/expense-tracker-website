4a89115026a44adb52ae7b725d0d78f9a02aabd1
Rutuja Mamankar <rutujamamankar407@gmail.com>
2025-09-19 22:51:47 +0530

    api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
 const API = axios.create({
-  baseURL:"https://expense-tracker-website-1-z9y1.onrender.com",
+  baseURL: BASE,
 });
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
+    req.headers = req.headers || {};
     req.headers.Authorization = `Bearer ${token}`;
   }
   return req;
 });
-
 export default API;
